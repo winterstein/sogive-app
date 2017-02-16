335a405c788c5d9e4d2a6f97687b514d5a07c642
Daniel Appel <daniel.appel.winterwell@gmail.com>
2017-02-16 17:11:19 +0000

    created a publishing script for the sogive app

---
+++ publish-sogiveapp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Publish-SoGive-App
+
+TARGETSERVERS=(heppner)
+
+
+# Are you sure that you want to do this?
+echo -e "\e[31;107m ARE YOU SURE THAT YOU WANT TO PUBLISH/UPDATE THE SOGIVE-APP TO THE FOLLOWING SERVER(S)? ($TARGETSERVERS) TO CANCEL PRESS \033[0m \e[30;107m ctl + c \033[0m \e[31;107m TO CONTINUE PRESS \033[0m \e[30;107m return \033[0m"
+read VAR
+if [[ -z $VAR ]]; then
+	echo -e "Proceeding..."
+fi
+
+for server in ${TARGETSERVERS[*]}; do
+	ssh winterwell@$server.soda.sh 'git --git-dir=/home/winterwell/sogive-app/.git/ --work-tree=/home/winterwell/sogive-app gc --prune=now'
+	ssh winterwell@$server.soda.sh 'git --git-dir=/home/winterwell/sogive-app/.git/ --work-tree=/home/winterwell/sogive-app pull origin master'
+	ssh winterwell@$server.soda.sh 'git --git-dir=/home/winterwell/sogive-app/.git/ --work-tree=/home/winterwell/sogive-app reset --hard FETCH_HEAD'
+	echo -e "$server updated"
+done
+echo -e "server(s) in array, $TARGETSERVERS updated"
