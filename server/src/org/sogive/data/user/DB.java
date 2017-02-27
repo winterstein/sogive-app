dc80cce9438107de2db793e4f941b1ededf0be36
Daniel Winterstein <daniel@sodash.com>
2017-02-27 21:45:45 +0000

    wip

--- server/src/org/sogive/data/user/DB.java
+++ server/src/org/sogive/data/user/DB.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.util.Map;
 import java.util.logging.Level;
 
+import org.sogive.data.charity.SoGiveConfig;
 
 import com.winterwell.utils.io.SqlUtils;
+import com.winterwell.es.ESType;
 import com.winterwell.es.client.ESHttpClient;
+import com.winterwell.es.client.IESResponse;
+import com.winterwell.es.client.admin.CreateIndexRequest;
+import com.winterwell.es.client.admin.PutMappingRequestBuilder;
 import com.winterwell.utils.Dependency;
 import com.winterwell.utils.io.ArgsParser;
 import com.winterwell.utils.io.DBUtils.DBOptions;
@@ -24,7 +29,22 @@ import com.winterwell.web.data.XId;
  */
 public class DB {
 
-	public void init() {
+	public static void init() {
+		ESHttpClient es = Dependency.get(ESHttpClient.class);
+		SoGiveConfig config = Dependency.get(SoGiveConfig.class);
+		
+		CreateIndexRequest pi = es.admin().indices().prepareCreate(config.donationIndex);
+		IESResponse r = pi.get();
+		
+		PutMappingRequestBuilder pm = es.admin().indices().preparePutMapping(config.donationIndex, "donation");
+		ESType dtype = new ESType();
+		dtype.property("from", new ESType().keyword());
+		dtype.property("to", new ESType().keyword());
+		dtype.property("time", new ESType().date());
+		pm.setMapping(dtype);
+		IESResponse r2 = pm.get();
+		r2.check();
+		
 //		DBOptions options = ArgsParser.getConfig(new DBOptions(), 
 //									new File("config/sogive.properties"));
 //		SqlUtils.setDBOptions(options);
