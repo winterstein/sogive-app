065f74acb1da3627273e40e1eb6c539480a65948
Daniel Winterstein <daniel@sodash.com>
2017-07-18 17:49:09 +0100

    more-careful-rep-project

--- src/data/charity/NGO.js
+++ src/data/charity/NGO.js
@@ -5,6 +5,14 @@ import {isa} from '../DataClass';
 import {assert} from 'sjtest';
 import Project from './Project';
 
+/**
+ * Each Charity (NGO -- which is the thing.org type) has projects.
+ * "overall" is a project.
+ * Each project has inputs and outputs. 
+ * Each output is augmented with impact data.
+ * There is a representative project -- this gives the impact that's reported.
+ */
+
 const NGO = {};
 export default NGO;
 
@@ -14,34 +22,40 @@ NGO.name = (ngo) => isa(ngo, 'NGO') && ngo.name;
 NGO.id = (ngo) => isa(ngo, 'NGO') && ngo['@id']; // thing.org id field
 NGO.description = (ngo) => isa(ngo, 'NGO') && ngo.description;
 
+/**
+ * @return {Project} the representative project
+ */
 NGO.getProject = (ngo) => {
 	NGO.isa(ngo);
-	const { projects } = ngo;
-
-	// Representative and ready for use?
-	const repProjects = _.filter(projects, (p) => (p.isRep && p.ready));
-
+	let projects = NGO.getProjects2(ngo);
 	// Get most recent, if more than one
 	let repProject = repProjects.reduce((best, current) => {
 		if ( ! current) return best;
 		if ( ! best) return current;
 		return best.year > current.year ? best : current;
 	}, null);
-
-	// ...or fall back.
-	if ( ! repProject) {
-		repProject = _.find(ngo.projects, p => p.name === Project.overall);
-	}
-
-	if ( ! repProject) {
-		repProject = ngo.projects && ngo.projects[0];
-	}
-
 	return repProject;
+};
+
+/**
+ * @return {Project[]}
+ */
+NGO.getProjects2 = (ngo) => {
+	const { projects } = ngo;
+	assert(_.isArray(projects), ngo);
+	// Filter for ready, and never show unready
+	let readyProjects = projects.filter(p => p.ready);
+
+	// Representative and ready for use?
+	const repProjects = readyProjects.filter(p => p.isRep);
+	if (repProjects.length) return repProjects;
+	
+	// ...or fall back.
+	let oProjects = readyProjects.filter(p => Project.isOverall(p));
+	if (oProjects.length) return oProjects;
+	
+	return readyProjects;
 };
 
 NGO.noPublicDonations = (ngo) => NGO.isa(ngo) && ngo.noPublicDonations;
 
-NGO.getImpacts = function(project) {
-
-};
--- src/data/charity/Project.js
+++ src/data/charity/Project.js
@@ -1,7 +1,8 @@
 /** Data model functions for the NGO data-type */
 
 import _ from 'lodash';
-import {isa} from '../DataClass';
+import assert from 'sjtest';
+import {isa, assIsa} from '../DataClass';
 
 const Project = {};
 export default Project;
@@ -9,9 +10,13 @@ export default Project;
 Project.overall = 'overall';
 Project.type = 'Project';
 
+
 Project.isa = (ngo) => isa(ngo, Project.type);
+Project.assIsa = (p) => assert(Project.isa(p));
 Project.name = (ngo) => isa(ngo, Project.type) && ngo.name;
 Project.year = (ngo) => isa(ngo, Project.type) && ngo.year;
+
+Project.isOverall = (project) => assIsa(project) && project.name === Project.overall;
 
 Project.make = function(base) {
 	let proj = {
