305052f872bda57001aa46c22ff3703bd6c4b191
Daniel Winterstein <daniel@winterwell.com>
2017-08-28 12:32:21 +0100

    sync

--- src/components/AccountPage.jsx
+++ src/components/AccountPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SJTest, {assert} from 'sjtest';
+import Login from 'you-again';
 import printer from '../utils/printer.js';
 import C from '../C';
 import Roles from '../Roles';
@@ -12,16 +13,13 @@ const AccountPage = () => {
 	return (
 		<div className=''>
 			<h2>My Account</h2>
+			<Misc.Card title='Login'>
+				ID: {Login.getId()}
+			</Misc.Card>
 			<Misc.Card title='Roles'>
 				<p>Roles determine what you can do. E.g. only editors can publish changes.</p>
 				{proles.value? proles.value.map(role => <RoleLine key={role} role={role} />) : <Misc.Loading />}
 			</Misc.Card>
-			<h3>In development...</h3>
-			<p>Thank you for joining SoGive at this early stage.
-				This is our first release, and there's still lots of work to do.
-				By the way, we release all our code as open-source. If you would
-				like to contribute to building SoGive, please get in touch.
-			</p>
 		</div>
 	);
 };
