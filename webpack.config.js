c4558070af9a0be7f780eb47644e1e8c47928d40
DA <daniel.appel.winterwell@gmail.com>
2017-09-04 18:14:10 +0100

    added babel-polyfill as an import, webpack process, and npm dependency.  it allows older browsers to use Object.values()

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: ["babel-polyfill", './src/app.jsx'],
   output: { path: __dirname, filename: './web/build/js/bundle.js' },
   devtool: 'source-map',
   resolve: {
