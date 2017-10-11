195a83009c0752b787f0d6afc4bcf419cecd6e2f
Daniel Winterstein <daniel@sodash.com>
2017-10-11 12:42:36 +0100

    sync

--- server/src/org/sogive/data/loader/CharityMatcher.java
+++ server/src/org/sogive/data/loader/CharityMatcher.java
@@ -7,6 +7,7 @@ import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.sogive.data.charity.NGO;
+import org.sogive.data.user.DB;
 
 import com.winterwell.es.client.ESConfig;
 import com.winterwell.es.client.ESHttpClient;
@@ -20,7 +21,15 @@ import com.winterwell.utils.Utils;
 public class CharityMatcher {
 
 	public ObjectDistribution<NGO> match(NGO ngo) {
+		ObjectDistribution<NGO> od = new ObjectDistribution<>();
 		// TODO search the DB by name, ID
+		// by ID?
+		List<NGO> charity = DB.getCharityById(ngo);
+		if (charity != null && ! charity.isEmpty()) {
+			od.train(charity);
+			return od;
+		}
+		
 		ESConfig ec = Dep.get(ESConfig.class);
 		ESHttpClient esjc = new ESHttpClient(ec);
 		SearchRequestBuilder search = esjc.prepareSearch("charity");
@@ -30,8 +39,7 @@ public class CharityMatcher {
 		QueryBuilder qb = QueryBuilders.simpleQueryStringQuery(q)
 							.defaultOperator(Operator.AND);
 		search.setQuery(qb);
-		List<Map> hits = search.get().getHits();
-		ObjectDistribution od = new ObjectDistribution<>();
+		List<Map> hits = search.get().getHits();		
 		Gson gson = Dep.get(Gson.class);
 		for (Map hit : hits) {
 			NGO ngoHit = gson.fromJson(gson.toJson(hit), NGO.class); 
--- server/src/org/sogive/data/user/DB.java
+++ server/src/org/sogive/data/user/DB.java
@@ -1,22 +1,34 @@
 package org.sogive.data.user;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.sogive.data.charity.NGO;
 import org.sogive.data.charity.SoGiveConfig;
+import org.sogive.data.loader.ImportOSCRData;
 
 import com.winterwell.utils.io.SqlUtils;
 import com.winterwell.data.KStatus;
 import com.winterwell.es.ESPath;
 import com.winterwell.es.ESType;
+import com.winterwell.es.client.ESConfig;
 import com.winterwell.es.client.ESHttpClient;
 import com.winterwell.es.client.IESResponse;
+import com.winterwell.es.client.SearchRequestBuilder;
 import com.winterwell.es.client.admin.CreateIndexRequest;
 import com.winterwell.es.client.admin.CreateIndexRequest.Analyzer;
+import com.winterwell.gson.Gson;
 import com.winterwell.es.client.admin.PutMappingRequestBuilder;
 import com.winterwell.utils.Dep;
+import com.winterwell.utils.StrUtils;
+import com.winterwell.utils.Utils;
 import com.winterwell.utils.io.ArgsParser;
 import com.winterwell.utils.log.Log;
 import com.winterwell.web.data.XId;
@@ -90,5 +102,25 @@ public class DB {
 		ESHttpClient es = Dep.get(ESHttpClient.class);
 		Map<String, Object> person = es.get("sogive", "user", id.toString());
 		return (Person) person;
+	}
+
+	public static List<NGO> getCharityById(NGO ngo) {
+		ESConfig ec = Dep.get(ESConfig.class);
+		ESHttpClient esjc = new ESHttpClient(ec);
+		SearchRequestBuilder search = esjc.prepareSearch("charity");
+		BoolQueryBuilder qb = QueryBuilders.boolQuery();
+		for (String f : new String[] {
+				NGO.ID, "englandWalesCharityRegNum", ImportOSCRData.OSCR_REG,
+				"niCharityRegNum",
+				"ukCompanyRegNum", "usCompanyRegNum"
+		}) {
+			if (ngo.get(f) != null) {
+				qb.should(QueryBuilders.termQuery(f, ngo.get(f)));
+			}
+		}
+		qb.minimumNumberShouldMatch(1);
+		search.setQuery(qb);
+		List<NGO> hits = search.get().getSearchResults(NGO.class);		
+		return hits;
 	}
 }
