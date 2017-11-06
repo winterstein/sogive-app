12e5c12c1916efceadde380b29721016185afba0
Roscoe <roscoe@winterwell.com>
2017-11-06 14:24:56 +0000

    Missing Basket code (front and back-end)

--- server/src/org/sogive/data/commercial/Basket.java
+++ server/src/org/sogive/data/commercial/Basket.java
@@ -1,7 +1,12 @@
 package org.sogive.data.commercial;
 
+import java.util.List;
+
+import org.sogive.data.charity.NGO;
+
 import com.winterwell.data.AThing;
 
 public class Basket extends AThing {
-
+	List<Ticket> tickets;
+	NGO charity;
 }
---
+++ src/data/Basket.js
@@ -0,0 +1,25 @@
+import {assert} from 'sjtest';
+import {isa} from './DataClass';
+import C from '../C';
+
+const Basket = {};
+export default Basket;
+
+Basket.type = C.TYPES.Basket;
+
+Basket.isa = (obj) => isa(obj, Basket.type);
+Basket.assIsa = (obj) => assert(Basket.isa(obj));
+
+Basket.getItems = (basket) => {
+	return basket.items || [];
+};
+
+Basket.make = (base = {}) => {
+	let ma = {
+		items: [],
+		...base,
+		'@type': Basket.type,
+	};
+	Basket.assIsa(ma);
+	return ma;
+};
