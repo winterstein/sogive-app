8ed9cddda8391d3627618c957dee6a0565163e12
Daniel Winterstein <daniel@winterwell.com>
2017-11-16 16:44:11 +0000

    no-unset

--- src/Settings.js
+++ src/Settings.js
@@ -9,3 +9,5 @@ if (navigator) {
 	if (navigator.languages && navigator.languages.length) Settings.locale = navigator.languages[0];
 	else Settings.locale = navigator.language;
 }
+// provide a default, 'cos otherwise NumberFormat gets upset
+if ( ! Settings.locale) Settings.locale = 'en-GB';
