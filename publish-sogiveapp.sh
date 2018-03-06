42f09d6749b9a8db47d89331e9a9d1e1f457b4b6
DA <daniel.appel.winterwell@gmail.com>
2018-03-06 11:49:59 +0000

    updated the publishing process to catch the difference between the testing-stripe-keys, and the production-stripe-keys and to make sure that an appropriate properties file gets to the appropriate targeted server

--- publish-sogiveapp.sh
+++ publish-sogiveapp.sh
@@ -79,6 +79,10 @@ for server in ${TARGET[*]}; do
 #	rsync -rhPe 'ssh -i ~/.ssh/winterwell@soda.sh' ~/winterwell/sogive-app/bin/* winterwell@$server:/home/winterwell/sogive-app/bin/
 	printf "\nsyncing everything else...\n"
 	rsync -rhPe 'ssh -i ~/.ssh/winterwell@soda.sh' --delete-before ~/winterwell/sogive-app/config/* winterwell@$server:/home/winterwell/sogive-app/config/
+	if [[ $server = 'hugh.soda.sh' ]]; then
+		ssh winterwell@$server 'rm /home/winterwell/sogive-app/config/stripe.properties'
+		scp ~/winterwell/logins/sogive-app/test.stripe.properties winterwell@$server:/home/winterwell/sogive-app/config/stripe.properties
+	fi
 	rsync -rhPe 'ssh -i ~/.ssh/winterwell@soda.sh' --delete-before ~/winterwell/sogive-app/data/* winterwell@$server:/home/winterwell/sogive-app/data/
 	rsync -rhPe 'ssh -i ~/.ssh/winterwell@soda.sh' --delete-before ~/winterwell/sogive-app/server/* winterwell@$server:/home/winterwell/sogive-app/server/
 	rsync -rhPe 'ssh -i ~/.ssh/winterwell@soda.sh' --delete-before ~/winterwell/sogive-app/src/* winterwell@$server:/home/winterwell/sogive-app/src/
