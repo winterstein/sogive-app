6f5d4d510ed9d792045c5377897f34283be9b9e4
Daniel Winterstein <daniel@winterwell.com>
2018-03-07 14:10:34 +0000

    sync

--- server/src/org/sogive/server/DonateToFundRaiserActor.java
+++ server/src/org/sogive/server/DonateToFundRaiserActor.java
@@ -11,6 +11,7 @@ import com.winterwell.data.KStatus;
 import com.winterwell.es.ESPath;
 import com.winterwell.es.IESRouter;
 import com.winterwell.utils.Dep;
+import com.winterwell.utils.Utils;
 import com.winterwell.utils.log.Log;
 import com.winterwell.utils.threads.Actor;
 import com.winterwell.web.app.AppUtils;
@@ -26,42 +27,60 @@ public class DonateToFundRaiserActor extends Actor<Donation> {
 	protected void consume(Donation donation, Actor from) throws Exception {
 		String frid = donation.getFundRaiser();
 		
-		updateFundRaiser(donation, frid, KStatus.DRAFT);
 		updateFundRaiser(donation, frid, KStatus.PUBLISHED);
-		
+		updateFundRaiser(donation, frid, KStatus.DRAFT);				
 	}
 	
+	/**
+	 * 
+	 * @param donation
+	 * @param frid
+	 * @param status draft or published version of the FundRaiser
+	 */
 	private void updateFundRaiser(Donation donation, String frid, KStatus status) {
-		Log.d(getName(), "updateFundRaiser "+donation+" frid: "+frid+" status: "+status);
-		ESPath path = Dep.get(IESRouter.class).getPath(FundRaiser.class, frid, status);
-		FundRaiser fundraiser = AppUtils.get(path, FundRaiser.class);
-				
-		Money amount = donation.getAmount();
-		
-		// Add in matched funding?
-		Event event = fundraiser.getEvent();
-		if (event != null && event.getMatchedFunding() != null && event.getMatchedFunding() != 0) {
-			double ma = amount.getValue100() * event.getMatchedFunding() / 100.0;
-			Money matchAmount = new Money(Math.round(ma), amount.getCurrency());
-			MoneyItem mi = new MoneyItem("matched funding", matchAmount.asMoney());
-			donation.addContribution(mi);
-			if (donation.getStatus() != KStatus.PUBLISHED) {
-				Log.w(getName(), "Not published?! "+donation+" to "+frid);
+		try {
+			Log.d(getName(), "updateFundRaiser "+donation+" frid: "+frid+" status: "+status);
+			ESPath path = Dep.get(IESRouter.class).getPath(FundRaiser.class, frid, status);
+			FundRaiser fundraiser = AppUtils.get(path, FundRaiser.class);
+					
+			Money amount = donation.getAmount();
+			
+			Throwable hackex = null;
+			// Add in matched funding?
+			try {
+				Event event = fundraiser.getEvent();
+				if (event != null && event.getMatchedFunding() != null && event.getMatchedFunding() != 0) {
+					double ma = amount.getValue100() * event.getMatchedFunding() / 100.0;
+					Money matchAmount = new Money(Math.round(ma), amount.getCurrency());
+					MoneyItem mi = new MoneyItem("matched funding", matchAmount.asMoney());
+					donation.addContribution(mi);
+					if (donation.getStatus() != KStatus.PUBLISHED) {
+						Log.w(getName(), "Not published?! "+donation+" to "+frid);
+					}				
+					if (status==KStatus.PUBLISHED) { // only save this the once 
+						AppUtils.doPublish(donation, false, true);
+					}
+				}			
+			} catch(Throwable ex) {
+				hackex = ex;
 			}
-			AppUtils.doPublish(donation, false, true);
+			
+			final Money prevTotal = donation.getTotal();
+			Money donated = fundraiser.getDonated();
+			if (donated == null) donated = Money.pound(0);
+			fundraiser.setDonated(donated.plus(prevTotal));
+			
+			Integer donationCount = fundraiser.getDonationCount();
+			if (donationCount == null) donationCount = 0;
+			fundraiser.setDonationCount(donationCount + 1);
+			// FIXME race condition vs edits or other donations!
+			// TODO use an update script, and handle conflict exceptions
+			Log.d(getName(), "updateFundRaiser count: "+fundraiser.getDonationCount()+" total: "+fundraiser.getDonated()+" from "+prevTotal);
+			AppUtils.doSaveEdit2(path, new JThing<FundRaiser>(fundraiser), null);
+			if (hackex != null) throw Utils.runtime(hackex);
+		} catch(Throwable ex) {
+			Log.e(getName(), ex);
+			throw Utils.runtime(ex);
 		}
-		
-		final Money prevTotal = donation.getTotal();
-		Money donated = fundraiser.getDonated();
-		if (donated == null) donated = Money.pound(0);
-		fundraiser.setDonated(donated.plus(prevTotal));
-		
-		Integer donationCount = fundraiser.getDonationCount();
-		if (donationCount == null) donationCount = 0;
-		fundraiser.setDonationCount(donationCount + 1);
-		// FIXME race condition vs edits or other donations!
-		// TODO use an update script, and handle conflict exceptions
-		Log.d(getName(), "updateFundRaiser count: "+fundraiser.getDonationCount()+" total: "+fundraiser.getDonated()+" from "+prevTotal);
-		AppUtils.doSaveEdit2(path, new JThing<FundRaiser>(fundraiser), null);
 	}	
 }
