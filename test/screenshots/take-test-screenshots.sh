4257b2ee34d5019166ac50c4e95f276e56bba84f
DA <daniel.appel.winterwell@gmail.com>
2018-04-09 13:26:17 +0100

    re-adjusted where puppeteer lives, and where the JS scripts to govern it live.

--- test/screenshots/take-test-screenshots.sh
+++ test/screenshots/take-test-screenshots.sh
@@ -9,7 +9,7 @@ else
 	sleep $1
 fi
 
-SOURCE_JS=("/home/$USER/winterwell/sogive-app/test/screenshots/compiled/test.sogive.org.js")
+SOURCE_JS=("/home/$USER/winterwell/sogive-app/test/screenshots/test.sogive.org.js")
 
 for js_file in ${SOURCE_JS[*]}; do
 	printf "\n"
