8e791ca29baa30fecbb8ce8a78b32d688937c81a
DA <daniel.appel.winterwell@gmail.com>
2018-04-09 14:07:06 +0100

    updated the publishing script so that the automated screenshots are sure to have all of the correct things in place before attempting to take the screenshots

--- publish-sogiveapp.sh
+++ publish-sogiveapp.sh
@@ -120,10 +120,18 @@ done
 
 printf "\nPublishing process completed\n"
 
+printf "\nGetting Ready to take Screenshots\n"
+if [ ! -d /home/$USER/winterwell/sogive-app/tests/screenshots/node_modules ]; then
+	cd /home/$USER/winterwell/sogive-app/tests/screenshots/ && npm i
+	cd /home/$USER/winterwell/sogive-app/tests/screenshots/ && bash compile.sh
+fi
+
 if [[ $PUBLISH_TYPE = 'test' ]]; then
+	cd /home/$USER/winterwell/sogive-app/tests/screenshots/ && bash compile.sh
 	printf "Taking screenshot of test.sogive.org in 10 seconds\n"
 	bash test/screenshots/take-test-screenshots.sh 10
 else
+	cd /home/$USER/winterwell/sogive-app/tests/screenshots/ && bash compile.sh
 	printf "Taking screenshot of app.sogive.org in 10 seconds\n"
 	bash test/screenshots/take-production-screenshots.sh 10
 fi
