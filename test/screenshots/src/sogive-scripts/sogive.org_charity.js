980295e43929311157724d9d50c7a2da90376145
Mark Wood <markw13a@gmail.com>
2018-05-11 13:05:58 +0100

    Notes on things to change

--- test/screenshots/src/sogive-scripts/sogive.org_charity.js
+++ test/screenshots/src/sogive-scripts/sogive.org_charity.js
@@ -1,14 +1,47 @@
 const puppeteer = require('puppeteer');
+const {timeout} = require('../res/UtilityFunctions');
 
 const DONATE_BUTTON = `#charity div.donate-button button`;
+const NEXT_BUTTON = `div.WizardStage div.nav-buttons.clearfix button`;
 
 /**Just a stub for now
  * Should go through donation process
  * Can choose to provide donor details
  */
 async function donate(args) {
-    const {page} = args;
+    const {
+        page, 
+        amount, 
+        giftAid,
+        details,
+        payment
+    } = args;
     await page.click(DONATE_BUTTON);
+    await timeout(1000);
+    await next({page});
+    //Each statement represents a stage of the form wizard
+    if(amount) {
+        //change donation amount
+    }
+    await page.once('load', async () => next({page}));
+    if(giftAid){
+        //switch to check various options
+    }
+    await page.once('load', async () => next({page}));
+    if(details){
+        //fill in form with corresponding details
+    }
+    await page.once('load', async () => next({page}));
+    if(payment){
+        //fill in payment form amd submit
+    }
+    //page.click("deliberately-break");
+}
+
+/**Advances through the donation form wizard */
+async function next(args) {
+    const {page} = args;
+    await page.click(NEXT_BUTTON);
 }
 
 module.exports = {
--- test/screenshots/src/test-manager.js
+++ test/screenshots/src/test-manager.js
@@ -10,7 +10,9 @@ const {onFail, writeToLog} = require('./res/UtilityFunctions');
  * Produce report of successes/failures of all tests run
  * How to decide if a final state should be counted as a success/failure?
  * Build on error given to include more detailed stack-trace
- * Basic log writing. Easier in console or from Node?
+ * Have tests time-out -- seen it get stuck a couple of times
+ * Possibly reattempt test? Best of three maybe? Balance between accuracy/speed to be struck
+ * How to marry .txt log and screenshots?
  */
 const headless = false;
 let browser;
@@ -22,41 +24,54 @@ const tests = [firstTest];
 const test_results = [];
 
 async function init() {
-    browser = await puppeteer.launch({headless});
-    page = await browser.newPage();
+	browser = await puppeteer.launch({headless});
+	page = await browser.newPage();
 }
 
 async function startTests() {
-    //Probably want to warn rather than throw error
-    //Might be very annoying if it shuts-down a time-consuming build process
-    if(!page) throw new Error('test-manager.js -- invalid page object. Check that init() has been called first');
-    if(!tests) throw new Error('test-manager.js -- no tests provided (tests[] is falsy)');
+	//Probably want to warn rather than throw error
+	//Might be very annoying if it shuts-down a time-consuming build process
+	if(!page) throw new Error('test-manager.js -- invalid page object. Check that init() has been called first');
+	if(!tests) throw new Error('test-manager.js -- no tests provided (tests[] is falsy)');
 
-    for (let i=0; i < tests.length; i++) {
-        try {
-            await tests[i].run(page);
-            test_results[i] = {
-                success : true
-            };
-        }
-        catch (e) {
-            onFail({error: e, page});
-            test_results[i] = {
-                success: false,
-                error: e,
-            };
-        }
-    }
+	for (let i=0; i < tests.length; i++) {
+		try {
+			await tests[i].run(page);
+			test_results[i] = {
+				success : true
+			};
+		}
+		catch (e) {
+			onFail({error: e, page});
+			test_results[i] = {
+				success: false,
+				error: e,
+			};
+		}
+	}
+}
+
+/**Currently just a wrapper for writeToLog
+ * Will expand to create a nice, human-readable log message
+ */
+function generateTestReport() {
+	if(!test_results) writeToLog(`\n${new Date().toISOString()}: 'test_results[] is blank'}`);
+	else writeToLog(`\n${new Date().toISOString()}: ${JSON.stringify(test_results)}`);        
 }
 
 async function run() {
-    await init();
-    await startTests();
-    await browser.close();
-    //Output test report
-    //Error thrown currently doesn't make clear where it's coming from
-    //Would be nice if it could be more descriptive for output to log
-    writeToLog(`\n${new Date().toISOString()}: ${JSON.stringify(test_results) || 'test_results[] is blank'}`);
+	await init();
+	await startTests();
+	//await browser.close();
+	generateTestReport();
 }
 
 run();
+
+//Script to run/compile
+//Alert if something's gone wrong
+//Simple "everything fine" if nothing wrong. Sends out "flag" with details if something's wrong (email to office).
+//true/false output for overall results
+//Move common non-site specific bits in symlink
+// Destructuring assignment in function args
+// Import instead of require
