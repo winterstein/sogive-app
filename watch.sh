e28f3314b8b2ca2055d5d3f1d5cc048353f6a997
Mark Wood <markw13a@gmail.com>
2018-05-28 15:26:40 +0100

    changed symlink webpack.config.js to a copy job on compile

--- watch.sh
+++ watch.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cp ../wwappbase.js/project/webpack.config.js webpack.config.js
 npm i &
 npm update --save &
 npm run compile-watch
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,25 @@
-../wwappbase.js/project/webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: ["babel-polyfill", './src/app.jsx'],
+  output: { path: __dirname, filename: './web/build/js/bundle.js' },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+	symlinks: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ["transform-object-rest-spread"]
+        }
+      }
+    ]
+  },
+};
