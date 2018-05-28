70bff87ba51d9aae46e9269a3fdf9f4b62bd3083
DA <daniel.appel.winterwell@gmail.com>
2018-05-28 11:56:16 +0100

    re-wrote the argument handlers in 'case' form, and added a check for NPM contingencies

--- test/jest.sh
+++ test/jest.sh
@@ -1,20 +1,61 @@
+#!/bin/bash
+
+
 JestOptionsBlob=""
-ENDPOINT=http://local.sogive.org 
-for var in $@
-do
-	if [ $var = "test" ]
-	then
-		ENDPOINT=https://test.sogive.org 		
-	elif [ $var = "local" ]
-	then
-		ENDPOINT=http://local.sogive.org 		
-	elif [ $var = "production" ]
-	then
-		ENDPOINT=https://app.sogive.org 		
-	else
-		JestOptionsBlob="$JestOptionsBlob $var"
-	fi
-done
+ENDPOINT='http://local.sogive.org'
+
+
+########
+### Handling Test Target arguments
+########
+case $1 in
+	test)
+	printf "\nGoing to run Jest tests on test.sogive.org\n"
+	ENDPOINT='https://test.sogive.org'
+	;;
+	production)
+	printf "\nGoing to run Jest tests on app.sogive.org\n"
+	ENDPOINT='https://app.sogive.org'
+	;;
+	local)
+	printf "\nGoing to run Jest tests on local.sogive.org\n"
+	ENDPOINT='http://local.sogive.org'
+	;;
+	*)
+	printf "\nGoing to run Jest tests on $1\n"
+	JestOptionsBlob="$JestOptionsBlob $1"
+esac
+
+
+
+
+
+
+
+# ENDPOINT=http://local.sogive.org 
+# for var in $@
+# do
+# 	if [ $var = "test" ]
+# 	then
+# 		ENDPOINT=https://test.sogive.org 		
+# 	elif [ $var = "local" ]
+# 	then
+# 		ENDPOINT=http://local.sogive.org 		
+# 	elif [ $var = "production" ]
+# 	then
+# 		ENDPOINT=https://app.sogive.org 		
+# 	else
+# 		JestOptionsBlob="$JestOptionsBlob $var"
+# 	fi
+# done
+
+
+########
+### Satisfy NPM contingencies
+#######
+printf "\nGetting NPM Packages to Run Jest Tests...\n"
+npm i
+
 
 RES=$(cd ~/winterwell/wwappbase.js/test-base/res/ && find -iname "*.js")
 #Jest will babel any test files itself,
