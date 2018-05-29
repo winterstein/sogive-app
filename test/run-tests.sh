f4a94ee320341790bd2a4809f21b0bd1a374353d
DA <daniel.appel.winterwell@gmail.com>
2018-05-29 14:57:34 +0100

    moved the checks for contingent software for the tests to the front of the publishing process

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -5,42 +5,6 @@
 ########
 EMAIL_RECIPIENTS='sysadmin@sodash.com'
 
-
-########
-### Check to see if you have mutt an msmtp installed so that you can send emails
-########
-if [[ $(which mutt) = '' ]]; then
-        printf "\nYou must first install mutt before running this script\nInstall mutt with\n\tsudo apt-get install mutt\n"
-        exit 0
-fi
-if [[ $(which msmtp) = '' ]]; then
-        printf "\nYou must first install msmtp before running this script\nInstall msmtp with\n\tsudo apt-get install msmtp\n"
-        exit 0
-fi
-
-
-########
-### Check to see if .msmtprc and .muttrc are in place
-########
-if [ ! -f ~/.msmtprc ]; then
-        printf "\nPlacing the .msmtprc file in your home directory so that I can send emails\n"
-        cp ~/winterwell/logins/.msmtprc ~/
-        chmod 600 ~/.msmtprc
-fi
-if [ ! -d ~/msmtp ]; then
-        printf "\nCreating a directory for the msmpt logs to live in\n"
-        mkdir -p ~/msmtp
-        touch ~/msmtp/msmtp.log
-        chmod 777 ~/msmtp/msmtp.log
-fi
-if [ ! -f ~/.muttrc ]; then
-        printf "\nPlacing the .muttrc file in your home directory so that I can send emails\n"
-        cp ~/winterwell/logins/.muttrc ~/
-        printf "\nYou must now run mutt for the first time in order to create a maildir\nRun mutt with \n\t$ mutt\n"
-        exit 0
-fi
-
-
 bash jest.sh $1
 
 
