914caaaecc683304c4742c1ed3f3f2b36904b2df
Daniel Winterstein <daniel@winterwell.com>
2018-06-18 10:39:57 +0100

    sync

--- server/build/PublishSoGiveApp.java
+++ server/build/PublishSoGiveApp.java
@@ -16,7 +16,7 @@ public class PublishSoGiveApp extends PublishProjectTask {
 	public PublishSoGiveApp() throws Exception {
 		super("sogive", "/home/winterwell/sogive-app");
 		bashScript = "./publish-sogiveapp.sh";
-		typeOfPublish = KPubType.test;
+		typeOfPublish = KPubType.production;
 //		codePart = "backend";
 //		compile = false;	
 	}
