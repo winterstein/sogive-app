bf667d1b3f73d5921d7813283d4e9a34b73c7a9a
DA <daniel.appel.winterwell@gmail.com>
2018-07-25 12:04:13 +0100

    have updated the publishing task to use the new BASH script.

--- builder/PublishSoGiveApp.java
+++ builder/PublishSoGiveApp.java
@@ -10,7 +10,13 @@ public class PublishSoGiveApp extends PublishProjectTask {
 	
 	public PublishSoGiveApp() throws Exception {
 		super("sogive", "/home/winterwell/sogive-app");
-		bashScript = "./publish-sogiveapp.sh";
+		//
+		// OLD LEGACY SCRIPT
+		//bashScript = "./publish-sogiveapp.sh";
+		//
+		// NEW SCRIPT
+		bashScript = "../wwappbase.js/./project-publisher.sh sogive";
+		// TODO: Get around this absolute path need.
 		typeOfPublish = KPubType.test;
 //		codePart = "backend";
 //		compile = false;	
