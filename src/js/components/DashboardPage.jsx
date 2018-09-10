ea65392b8ca278ec91afa3a47fa48656be71728c
Daniel Winterstein <daniel@winterwell.com>
2018-09-10 10:40:58 +0100

    Revert "wip"
    This reverts commit d78875982c6d1ef8290e47918a983b7678e163b4.

--- src/js/components/DashboardPage.jsx
+++ src/js/components/DashboardPage.jsx
@@ -11,15 +11,116 @@ import DataStore from '../base/plumbing/DataStore';
 // import ChartWidget from './../base/components/ChartWidget';
 import Misc from '../base/components/Misc';
 import {LoginLink} from '../base/components/LoginWidget';
+import Donation from '../data/charity/Donation';
+import CharityPageImpactAndDonate, {DonateButton} from './DonationWizard';
 
 const DashboardPage = () => {
+	let user = Login.getUser();
+	if ( ! user) {
+		return (
+			<div className="page DashboardPage">
+				<h2>My Dashboard</h2>
+				<div><LoginLink title='Login or Register' /> to track your donations</div>
+			</div>
+		);
+	}
+
+	const pv = user? DataStore.fetch(['list','Donation','dashboard'],	
+		() => {
+			return ServerIO.getDonations({from:Login.getId()})
+				.then(function(result) {
+					let dons = result.cargo.hits;
+					return dons;
+				});
+		}) : {};
+	const donations = pv.value;
+
+	if ( ! donations && ! pv.error) {		
+		return (
+			<div className="page DashboardPage">
+				<h2>My Dashboard</h2>
+				<Misc.Loading />
+			</div>
+		);
+	}
 	// display...
 	return (
 		<div className="page DashboardPage">
 			<h2>My Dashboard</h2>
+			<div>
+				<DashboardWidget title="Donation History">
+					<DonationList donations={donations} />				
+				</DashboardWidget>
+				{donations.length? null : 
+					<DashboardWidget title='Welcome to SoGive'>
+						Get started by <a href='/#search'>searching</a> for a charity.
+					</DashboardWidget>
+				}
+			</div>
 		</div>
 	);
 }; // ./DashboardPage
 
+
+const DonationList = ({donations}) => {
+	return <div>{ _.map(donations, d => <DonationListItem key={'d'+d.id} donation={d} />) }</div>;
+};
+
+const DonationListItem = ({donation}) => {
+	let charityId = donation.to;
+	// TODO fetch charity info
+	let niceName = charityId.split('-');
+	niceName = niceName.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+	niceName = niceName.join(' ');
+	const impact = donation.impact? <div>Your donation funded {printer.prettyNumber(donation.impact.count, 2)} {donation.impact.unit}</div> : null;
+	return (
+		<div className='well'>
+			<Misc.Time time={donation.date} />
+			You donated <Misc.Money precision={false} amount={Donation.amount(donation)} /> 
+			to <a href={'#charity?charityId='+encURI(charityId)}>{niceName}</a>
+			{donation.fundRaiser && donation.via? <span> as part of <a href={'#fundraiser/'+encURI(donation.fundRaiser)}>{XId.prettyName(donation.via)}'s fund-raiser</a></span> : null}
+			.
+			{impact}
+			<div>GiftAid? {donation.giftAid? 'yes' : 'no'} <br />
+			<small>Payment ID: {donation.paymentId}</small></div>
+		</div>
+	);
+};
+
+		/*<h2>Version 2+...</h2>
+		<DashboardWidget title="News Feed">
+			Updates from projects you support and people you follow.
+		</DashboardWidget>
+
+		<DashboardWidget title="Your Donations over Time">
+			<ChartWidget type="line" />
+		</DashboardWidget>
+
+		<DashboardWidget title="Donations by Category">
+			Pie chart of what you give to
+		</DashboardWidget>
+
+		<DashboardWidget title="Your Badges">
+			Badges (encouraging use of all features, and repeated use -- but not extra £s)
+		</DashboardWidget>
+
+		<DashboardWidget title="Recent Donations">
+			List of recent donations and impacts, with a link to the full history
+		</DashboardWidget>*/
+
+
+const DashboardWidget = ({ children, iconClass, title }) =>
+	(<div className="panel panel-default">
+		<div className="panel-heading">
+			<h3 className="panel-title"><DashTitleIcon iconClass={iconClass} /> {title || ''}</h3>
+		</div>
+		<div className="panel-body">
+			{children}
+		</div>
+</div>);
+// ./DashboardWidget
+
+const DashTitleIcon = ({ iconClass }) =>
+	<i className={iconClass} aria-hidden="true" />;
 
 export default DashboardPage;
--- src/js/plumbing/ServerIO.js
+++ src/js/plumbing/ServerIO.js
@@ -9,6 +9,7 @@ import {XId, encURI} from 'wwutils';
 import C from '../C.js';
 
 import Login from 'you-again';
+import NGO from '../data/charity/NGO';
 
 // Try to avoid using this for modularity!
 import DataStore from '../base/plumbing/DataStore';
@@ -24,5 +25,64 @@ ServerIO.APIBASE = '';
 window.wtf = 'foo';
 
 ServerIO.checkBase();
+
+ServerIO.getServletForType = (type) => {
+	if (C.TYPES.isNGO(type)) {
+		return 'charity';
+	}
+	return type.toLowerCase();
+};
+
+/**
+ * @param query {!String} query string
+ * @param status {?KStatus} optional to request draft
+ */
+ServerIO.searchCharities = function({q, prefix, from, size, status, recommended}) {
+	// assMatch( q || prefix, String);
+	return ServerIO.load('/search.json', {data: {q, prefix, from, size, status, recommended}} );
+};
+
+
+ServerIO.getCharity = function(charityId, status) {
+	return ServerIO.getDataItem({type: C.TYPES.NGO, id: charityId, status: status});
+};
+
+
+ServerIO.getDonations = function({from, to, fundRaiser, status=C.KStatus.PUBLISHED}) {		
+	const params = {
+		data: {
+			from, to,
+			status,
+			sort:'date-desc'
+		}
+	};
+	// if (fundRaiser) { TODO
+	// 	assMatch(fundRaiser, String);
+	// 	params.data.q = 'fundRaiser:'+fundRaiser;
+	// }
+	return ServerIO.load('/donation/_list.json', params);
+};
+
+/**
+ * TODO handle charity or fundraiser
+ */
+ServerIO.getDonationDraft = ({from, charity, fundRaiser}) => {
+	assMatch(charity || fundRaiser, String);
+	let to = charity;
+	let q = fundRaiser? "fundRaiser:"+fundRaiser : null;
+	let status = C.KStatus.DRAFT;
+	return ServerIO.load('/donation/_list.json', {data: {from, to, q, status}, swallow: true});
+};
+
+
+/**
+ * @param charity {name:String}
+ */
+ServerIO.addCharity = function(charity, status=C.KStatus.DRAFT) {
+	let params = {		
+		data: {action: 'new', item: JSON.stringify(charity), status: status},
+		method: 'PUT'};
+	return ServerIO.load('/charity.json', params);
+};
 
 export default ServerIO;
