e55702aa35c89c3d2c99fdbde5e6f12b1e72f2b8
DW-Stross <daniel@good-loop.com>
2018-11-22 16:11:34 +0000

    publish-oddities

--- builder/PublishSoGiveApp.java
+++ builder/PublishSoGiveApp.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.util.List;
 
 import com.winterwell.bob.BuildTask;
-//import com.winterwell.calstat.BuildCalstat;
-import com.winterwell.utils.io.FileUtils;
 import com.winterwell.web.app.build.KPubType;
 import com.winterwell.web.app.build.PublishProjectTask;
 
