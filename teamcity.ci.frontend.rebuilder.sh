48ca2e3ebfb18e0f9207bdaa8cd35a8d78cc8916
DA <daniel.appel.winterwell@gmail.com>
2019-12-04 12:47:37 +0000

    added a teamcity frontend rebuilder script

---
+++ teamcity.ci.frontend.rebuilder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET_SERVER='simmons.soda.sh'
+TARGET_DIR='/home/winterwell/sogive-app/'
+SYNC_LIST=("server" "src" "web" "package.json" "webpack.config.js")
+
+for sync_item in $SYNC_LIST(@); do
+	rsync -rLhP $sync_item winterwell@$TARGET_SERVER:$TARGET_DIR
+done
+
+ssh winterwell@$TARGET_SERVER "cd $TARGET_DIR && npm i"
+ssh winterwell@$TARGET_SERVER "cd $TARGET_DIR && npm run compile"
+ssh winterwell@$TARGET_SERVER "cd $TARGET_DIR && bash ./convert.less.sh"
