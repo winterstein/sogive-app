5c1d1ff6435445fa6b450708cc62d2173eb2f6df
DA <daniel.appel.winterwell@gmail.com>
2019-12-06 12:07:54 +0000

    fixed the undefined variable issue in the teamcity script.  appbase should now symlink

--- teamcity.ci.frontend.rebuilder.sh
+++ teamcity.ci.frontend.rebuilder.sh
@@ -2,13 +2,13 @@
 
 TARGET_SERVER='hugh.soda.sh'
 TARGET_DIR='/home/winterwell/sogive-app/'
-SYNC_LIST=("server" "src" "web" "package.json" "webpack.config.js")
+SYNC_LIST=("src" "web" "package.json" "webpack.config.js")
 
 
 printf "\nRemoving old 'base' symlink\n"
-rm -rf $PROJECT_LOCATION/src/js/base
+rm -rf src/js/base
 printf "\nSymlinking 'base' from where TeamCity keeps it's wwappbase.js repo\n"
-ln -s /home/winterwell/TeamCity/buildAgent/work/9307b27f248c307/base $PROJECT_LOCATION/src/js/base
+ln -s /home/winterwell/TeamCity/buildAgent/work/9307b27f248c307/base src/js/base
 
 for sync_item in ${SYNC_LIST[@]}; do
 	rsync -rLhP $sync_item winterwell@$TARGET_SERVER:$TARGET_DIR
