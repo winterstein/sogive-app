412a076160e953ef3397bf36be9696283a396d7d
DA <daniel.appel.winterwell@gmail.com>
2020-03-12 17:40:03 +0000

    bug fixing the formatting of the summized output for the automated-tests-summary-email

--- run-tests.sh
+++ run-tests.sh
@@ -8,9 +8,9 @@
 # 03. email number and summary of errors to Andris
 
 PROJECT_NAME="sogive-app"
-RECEIVERS=(andris@good-loop.com)
-LOGFILE="~/winterwell/sogive-app/sogive.tests.output.log"
-SUMMARY_LOGFILE="~/winterwell/sogive-app/sogive.failed.tests.summary.log"
+RECEIVERS=(sysadmin@good-loop.com)
+LOGFILE='sogive.tests.output.log'
+SUMMARY_LOGFILE='sogive.failed.tests.summary.log'
 
 function send_email {
     printf "\n$EMAIL_BODY\n" | mutt -s "$SUBJECT" $RECEIVERS
@@ -19,10 +19,16 @@ function send_email {
 #clear out any residual test-logs that might exist in the dir
 if [ -f $LOGFILE ]; then
     rm $LOGFILE
+    touch $LOGFILE
+else
+    touch $LOGFILE
 fi
 
 if [ -f $SUMMARY_LOGFILE ]; then
     rm $SUMMARY_LOGFILE
+    touch $SUMMARY_LOGFILE
+else
+    touch $SUMMARY_LOGFILE
 fi
 
 # Get node console out put into a text file
@@ -34,7 +40,7 @@ NUM_FAILS=$(grep "FAIL" $LOGFILE | wc -l)
 
 # Get the full stack of the failed test(s):
 if [[ $NUM_FAILS -gt '0' ]]; then
-    grep "FAIL" tests.console.output.log -A15 >> $SUMMARY_LOGFILE
+    grep -v "PASS" $LOGFILE | grep -v "npm" | grep -v "Test" | grep -v "Snapshots" | grep -v "Time" | grep -v "Ran all test suites" >> $SUMMARY_LOGFILE
 fi
 
 # If there are any failures detected, send the alert email
