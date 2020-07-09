5f939f16105073a36e38f6a2cc3093744298a250
Daniel Winterstein <daniel@good-loop.com>
2020-07-09 22:13:48 +0100

    print.css

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -9,6 +9,7 @@ import MainDiv from './components/MainDiv';
 
 // Import root LESS file so webpack finds & renders it out to main.css
 import '../style/main.less';
+import '../style/print.less';
 
 // Pass font awesome version onto Misc, so it adds the appropiate class to the icons
 Misc.FontAwesome = 5;
