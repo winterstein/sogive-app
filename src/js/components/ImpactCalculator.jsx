5bb0b30cb34a32c24a17c552bad2ebd1577eac20
Aishah <aissshah@outlook.com>
2021-01-26 10:54:33 +0000

    Create border class

--- src/js/components/ImpactCalculator.jsx
+++ src/js/components/ImpactCalculator.jsx
@@ -110,7 +110,7 @@ class ImpactCalculator extends Component {
 		const donationUp = () => this.incrementDonation(amount, 1, charity);
 
 		return (
-			<div className='donation-impact'>
+			<div className='donation-impact border'>
 				<button onClick={donationDown} className='donation-down'>-</button>
 				<Misc.PropControl type='Money' prop='amount' path={formPath} changeCurrency={false} />
 				<button onClick={donationUp} className='donation-up'>+</button>
