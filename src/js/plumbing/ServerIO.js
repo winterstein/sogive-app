c25846bd6d15baadbdb27d179869b57098da4a31
Anita Woodruff <anita.woodruff@gmail.com>
2021-06-08 09:14:18 +0100

    Point frontend to local backend

--- src/js/plumbing/ServerIO.js
+++ src/js/plumbing/ServerIO.js
@@ -16,8 +16,8 @@ import ServerIO from '../base/plumbing/ServerIOBase';
 import KStatus from '../base/data/KStatus.js';
 
 ServerIO.APIBASE = '';
-ServerIO.APIBASE = 'https://test.sogive.org';
-ServerIO.APIBASE = 'https://app.sogive.org';
+// ServerIO.APIBASE = 'https://test.sogive.org';
+// ServerIO.APIBASE = 'https://app.sogive.org';
 
 // ?? use media.good-loop.com??
 ServerIO.MEDIA_ENDPOINT = '/upload.json';
