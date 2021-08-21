1974cdbcf1984fdab18f7fccfe39b8429bd045ee
Anita Woodruff <anita.woodruff@gmail.com>
2021-08-21 11:08:51 +0100

    boldify rating names in Learn More modal

--- src/js/components/LearnAboutRatings.jsx
+++ src/js/components/LearnAboutRatings.jsx
@@ -12,13 +12,13 @@ export const LearnAboutRatings = ({isButton}) => {
 				<ModalHeader toggle={toggle}>Our ratings</ModalHeader>
 				<ModalBody>
 					<ul>
-						<li>Gold means we have high confidence and strong evidence that the charity’s work is effective, and that its cost-effectiveness outperforms the Gold Standard.</li>
-						<li>Silver means that we have high confidence that the charity’s work is effective, though its cost-effectiveness likely falls behind the Gold Standard.</li>
-						<li>Bronze means that we have high confidence that the charity’s work is effective, though its cost-effectiveness likely falls well behind the Gold Standard.</li>
-						<li>Not recommended means that we do not have good evidence that the charity’s work is effective, and that we do have confidence that its cost-effectiveness underperforms the Gold Standard.</li>
-						<li>Too Rich means we believe the charity already has lots of money, so donations will likely have minimal impact for some time.</li>
-						<li>More info needed means we do not have good evidence that the charity’s work is effective, nor do we have an indication of its cost-effectiveness.</li> 
-						<li>More info needed (promising) means we do not have sufficient evidence that the charity’s work is effective, nor do we have sufficient indication of its cost-effectiveness. However, we have some reason to think the charity’s efficacy might be promising, pending additional information.</li>
+						<li><b>Gold</b> means we have high confidence and strong evidence that the charity’s work is effective, and that its cost-effectiveness outperforms the Gold Standard.</li>
+						<li><b>Silver</b> means that we have high confidence that the charity’s work is effective, though its cost-effectiveness likely falls behind the Gold Standard.</li>
+						<li><b>Bronze</b> means that we have high confidence that the charity’s work is effective, though its cost-effectiveness likely falls well behind the Gold Standard.</li>
+						<li><b>Not recommended</b> means that we do not have good evidence that the charity’s work is effective, and that we do have confidence that its cost-effectiveness underperforms the Gold Standard.</li>
+						<li><b>Too rich</b> means we believe the charity already has lots of money, so donations will likely have minimal impact for some time.</li>
+						<li><b>More info needed</b> means we do not have good evidence that the charity’s work is effective, nor do we have an indication of its cost-effectiveness.</li>
+						<li><b>More info needed (promising)</b> means we do not have sufficient evidence that the charity’s work is effective, nor do we have sufficient indication of its cost-effectiveness. However, we have some reason to think the charity’s efficacy might be promising, pending additional information.</li>
 					</ul>
 				</ModalBody>
 			</Modal>
