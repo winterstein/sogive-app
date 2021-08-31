87874ba71db5697e1188960f44d79a78f62e0d85
Daniel Winterstein <daniel@good-loop.com>
2021-08-31 13:03:11 +0100

    allow blank strings - adjust csv import

--- src/java/org/sogive/data/loader/Editorials.java
+++ src/java/org/sogive/data/loader/Editorials.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
+/**
+ * @deprecated Why not use a plain-old List??
+ */
 public class Editorials implements Iterable<Editorial> {
 	private final Collection<Editorial> charityEditorials;
 
