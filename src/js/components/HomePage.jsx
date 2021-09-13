e579be8a1199d4a73c9b18a766a7bb4fddbd2309
Wing Wong <wing@good-loop.com>
2021-09-13 17:58:17 +0100

    sync

--- src/js/components/HomePage.jsx
+++ src/js/components/HomePage.jsx
@@ -16,14 +16,16 @@ const HeroBanner = () => {
     return (
         <div className="gradient">
             <div className="worldImage">
-                <div className="row">
-                    <div className="col-6">
-                        <h2>Donate to Charity More Effectively</h2>
-                        <p>SoGive researches which charities achieve outsized impact through analysing independent evaluations and financial data.</p>
-                        <a href="#search" className="btn">Explore impact ratings</a>
+                <div className="row mx-5 p-5">
+                    <div className="col-6 mt-5">
+                        <div className="">
+                            <h2>Donate to Charity More Effectively</h2>
+                            <p>SoGive researches which charities achieve outsized impact through analysing independent evaluations and financial data.</p>
+                            <a href="#search" className="btn explore-impact-ratings p-3">Explore impact ratings</a>
+                        </div>
                     </div>
-                    <div className="col-6">
-                        <h2>Insert Images Here</h2>
+                    <div className="col-6 mt-5">
+                        <img className="h-50" src="/img/homepage/banner-cards.png" />
                     </div>
                 </div>
             </div>
