17df2e705e92e65b8d098f52b242bf9d80465859
Daniel Winterstein <daniel@good-loop.com>
2023-01-18 14:05:06 +0000

    feature/2023-01/limit-search-to-certain-fields

---
+++ test/java/org/sogive/server/CharityServletTest.java
@@ -0,0 +1,16 @@
+package org.sogive.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * {@link CharityServlet}
+ * See NGOClientTest
+ * @author daniel
+ *
+ */
+public class CharityServletTest {
+
+
+}
